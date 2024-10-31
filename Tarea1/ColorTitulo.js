6fc4a3eb0425a2cbec99803c1b033aa74cf142c9
Luis E. Hernández <47158448+Luis-Herz@users.noreply.github.com>
2024-10-30 22:39:33 -0600

    Add files via upload

---
+++ Tarea1/ColorTitulo.js
@@ -0,0 +1,19 @@
+const titulo1 = document.querySelector(".cabecera");
+/*
+const bton = document.querySelector("button");
+bton.addEventListener("click", function cambiarColor(color){
+    if (titulo1.style.color != color){
+        titulo1.style.color = color;
+    } else {
+        titulo1.style.color = 'black';
+    }
+});
+*/
+function cambiarColor(color){
+    if (titulo1.style.color != color){
+        titulo1.style.color = color;
+    } else {
+        titulo1.style.color = 'black';
+    }
+}
+
