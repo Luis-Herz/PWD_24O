7b0872780bc07b18465fe8d1063f9db249bbfca5
Luis-Herz <2153068030@cua.uam.mx>
2024-11-04 17:08:23 -0600

    Ejercicio 2 añadido
    Se agregó un párrafo en el html, este párrafo contiene el icono de un globo que al hacerle clic aumenta de tamaño, después de 8 clics el icono cambia por el de una explosión.

--- Tarea1/ColorTitulo.js
+++ Tarea1/ColorTitulo.js
@@ -1,14 +1,5 @@
 const titulo1 = document.querySelector(".cabecera");
-/*
-const bton = document.querySelector("button");
-bton.addEventListener("click", function cambiarColor(color){
-    if (titulo1.style.color != color){
-        titulo1.style.color = color;
-    } else {
-        titulo1.style.color = 'black';
-    }
-});
-*/
+
 function cambiarColor(color){
     if (titulo1.style.color != color){
         titulo1.style.color = color;
@@ -16,4 +7,16 @@ function cambiarColor(color){
         titulo1.style.color = 'black';
     }
 }
+
+const globo = document.getElementById("globo");
+let tamano = 15;
+globo.addEventListener("click", function cambiaTam(){
+    if(globo.style.fontSize >= "80px"){
+        globo.textContent = "💥";
+    } else {
+        tamano = tamano + 10;
+        globo.style.fontSize = tamano + "px";
+        console.log(tamano);
+    }
+});
 
